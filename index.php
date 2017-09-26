9c9e9f524508921121e7b0eb80bed5e6485739f5
Shriya S Samak <sss329@afsconnect1.njit.edu>
2017-09-26 17:09:41 -0400

    array merge

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
   $array = array(1,2,3,4,5,6,7);
   $obj->printArray($array);
 
+  $array1= array("Shriya","Pune","Bhakharwadi");
+  $array2 = array(8,9);
+  $obj->mergeArray($array,$array1);
+
   class main {
  
 	public function __construct() {
@@ -25,7 +29,14 @@
 		print_r($array);
                 echo '<hr>';
        }
-										    
+	
+	public function mergeArray($array1,$array2)	{
+		echo '<h1> Merge Array Function </h1>';
+		$result = array_merge($array1,$array2);
+		print_r($result);
+		echo '<hr>';
+	}
+
         public function __destruct() {
 
 		echo '</br> I\'m Done';	
